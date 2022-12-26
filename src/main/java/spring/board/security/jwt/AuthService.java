832196b9a5006fc6d8580144556bf434159363e6
HJ_Kim <ruby45342@gmail.com>
2022-12-26 17:48:00 +0900

    when tokenRefresh check logout

--- src/main/java/spring/board/security/jwt/AuthService.java
+++ src/main/java/spring/board/security/jwt/AuthService.java
@@ -13,8 +13,6 @@ import spring.board.web.dto.RefreshToken;
 import spring.board.web.dto.UserResponseDto;
 import spring.board.web.dto.UserTokenDto;
 
-import java.util.Optional;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -28,10 +26,7 @@ public class AuthService {
     public UserTokenDto login(UserResponseDto userResponseDto) {
 
         // Login ID/PW 를 기반으로 인증 체크
-        Optional<Users> userObject = userRepository.findByEmail(userResponseDto.getEmail());
-        UserDetailsVO authentication;
-
-        authentication = userObject.map(UserDetailsVO::new).orElse(null);
+        UserDetailsVO authentication = userRepository.findByEmail(userResponseDto.getEmail()).map(UserDetailsVO::new).orElse(null);
 
         UserTokenDto tokenDto = null;
         try {
@@ -52,26 +47,33 @@ public class AuthService {
     @Transactional
     public UserTokenDto reissue(UserTokenDto oldToken) {
         // 1. Refresh Token 검증
-        if (!tokenProvider.validateToken(oldToken.getRefreshToken())) throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+        if (!tokenProvider.validateToken(oldToken.getRefreshToken())) {
+            log.warn("Refresh Token 이 유효하지 않습니다.");
+            return null;
+        }
 
         // 2. Access Token 에서 User 정보 가져오기
         Authentication authentication = tokenProvider.getAuthentication(oldToken.getAccessToken());
 
         // 3. 저장소에서 User email 을 기반으로 Refresh Token 값 가져옴
         RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName());
-        if (refreshToken == null) throw new RuntimeException("로그아웃 된 사용자입니다.");
 
         // 4. Refresh Token 일치하는지 검사
-        if (!refreshToken.getValue().equals(oldToken.getRefreshToken())) throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+        if (refreshToken == null) log.warn("로그아웃 된 사용자입니다.");
+        else if (!refreshToken.getValue().equals(oldToken.getRefreshToken())) log.warn("토큰의 유저 정보가 일치하지 않습니다.");
+        else {
+            // 5. 새로운 토큰 생성
+            Users user = userRepository.findByEmail(authentication.getName()).get();
+            UserTokenDto tokenDto = tokenProvider.generateTokenDto(new UserDetailsVO(user));
+            // 6. 저장소 정보 업데이트
+            RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
+            refreshTokenRepository.save(newRefreshToken);
+            log.info("토큰이 갱신되었습니다.");
 
-        // 5. 새로운 토큰 생성
-        Users user = userRepository.findByEmail(authentication.getName()).get();
-        UserTokenDto tokenDto = tokenProvider.generateTokenDto(new UserDetailsVO(user));
-        // 6. 저장소 정보 업데이트
-        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
-        refreshTokenRepository.save(newRefreshToken);
+            // 토큰 발급
+            return tokenDto;
+        }
 
-        // 토큰 발급
-        return tokenDto;
+        return null;
     }
 }
